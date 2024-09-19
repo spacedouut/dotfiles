6016eeaf7738ef9baa19445ca5a78b8ccc4b154e
unk0wn <73479844+notunk0wn23@users.noreply.github.com>
2024-09-19 11:44:17 -0400

    Delete .bin directory

--- .bin/reload_colors.sh
+++
@@ -1,4 +0,0 @@
-matugen image ~/.cache/current_wallpaper.jpg --mode $(cat ~/.cache/user_active_theme)
-
-ags --run-js "applyStyle();"
-gradience-cli apply -p ~/.cache/matugen/compiled/gradience.json --gtk both
--- .bin/update_wallpaper.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-WALLPAPER_FOLDER="/home/spaced/.config/wallpapers/"
-WALLPAPER=$(find $WALLPAPER_FOLDER -type f | shuf -n 1)
-echo "$WALLPAPER"
-cp "$WALLPAPER" ~/.cache/current_wallpaper.jpg
-~/.bin/reload_colors.sh
-
-
